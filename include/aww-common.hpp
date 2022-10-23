dc8cfb1f87c0383995f6374a5272a32d0f0e05e0
Gogs <gogs@fake.local>
2022-10-23 13:52:42 -0700

    formatting and error handling changes

--- include/aww-common.hpp
+++ include/aww-common.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#ifndef AWW_COMMON_HPP
+#define AWW_COMMON_HPP
 
 #include <string>
 #include <vector>
@@ -8,21 +10,33 @@
 #include <ctime>
 #include <stdlib.h>
 
-namespace aww {
+namespace aww
+{
 
-const int result = 0;
-const int error = 0;
+  const int resultPos = 0;
+  const int errorPos = 1;
+
+  typedef std::tuple<bool, std::string> result_t;
+
+  bool failed(const result_t&);
+  bool succeeded(const result_t&);
+  std::string make_error(const std::string&, const result_t&);
 
 } // namespace aww
 
-namespace aww::date {
+namespace aww::date
+{
   std::string getDateYYYYMMDD(void);
 }
 
-namespace aww::os::actions {
-    std::tuple<bool, std::string> launchFile(const std::string&);
+namespace aww::os::actions
+{
+  aww::result_t launchFile(const std::string &);
 }
 
-namespace aww::string {
-  std::string join(const std::vector<std::string>&, const std::string&);
+namespace aww::string
+{
+  std::string join(const std::vector<std::string> &, const std::string &);
 }
+
+#endif // AWW_COMMON_HPP
--- src/aww-common-windows.cpp
+++ src/aww-common-windows.cpp
@@ -2,61 +2,78 @@
 
 #include "Windows.h"
 
-namespace aww::os::actions {
+namespace aww::os::actions
+{
 
-    std::tuple<bool, std::string> launchFile(const std::string& path) {
-        // check path is null
-        if (path.empty()) {
-            return std::make_tuple(false, "Argument path is empty");
-        }
-        HINSTANCE execResult = ShellExecuteA(nullptr, "open", path.c_str(), nullptr, nullptr, SW_SHOWNORMAL);
-        const HINSTANCE Success = reinterpret_cast<HINSTANCE>(33);
-
-        if (execResult >= Success) {
-            return std::make_tuple(true, "");
-        }
-
-        unsigned long errorCode = GetLastError();
-
-        if (errorCode == 0) {
-            return std::make_tuple(false, "ShellExecuteA failed: The operating system is out of memory or resources.");
-        } else if (errorCode == ERROR_FILE_NOT_FOUND)
-        {
-            return std::make_tuple(false, "ShellExecuteA failed: The specified file was not found.");
-        } else if (errorCode == ERROR_PATH_NOT_FOUND)
-        {
-            return std::make_tuple(false, "ShellExecuteA failed: The specified path was not found.");
-        } else if (errorCode == ERROR_BAD_FORMAT)
-        {
-            return std::make_tuple(false, "ShellExecuteA failed: The .exe file is invalid (non-Win32 .exe or error in .exe image).");
-        } else if (errorCode == SE_ERR_ACCESSDENIED)
-        {
-            return std::make_tuple(false, "ShellExecuteA failed: The operating system denied access to the specified file.");
-        } else if (errorCode == SE_ERR_ASSOCINCOMPLETE)
-        {
-            return std::make_tuple(false, "ShellExecuteA failed: The file name association is incomplete or invalid.");
-        } else if (errorCode == SE_ERR_DDEBUSY)
-        {
-            return std::make_tuple(false, "ShellExecuteA failed: The DDE transaction could not be completed because other DDE transactions were being processed.");
-        } else if (errorCode == SE_ERR_DDEFAIL)
-        {
-            return std::make_tuple(false, "ShellExecuteA failed: The DDE transaction failed.");
-        } else if (errorCode == SE_ERR_DDETIMEOUT)
-        {
-            return std::make_tuple(false, "ShellExecuteA failed: The DDE transaction could not be completed because the request timed out.");
-        } else if (errorCode == SE_ERR_DLLNOTFOUND)
-        {
-            return std::make_tuple(false, "ShellExecuteA failed: The specified DLL was not found.");
-        } else if (errorCode == SE_ERR_NOASSOC)
-        {
-            return std::make_tuple(false, "ShellExecuteA failed: There is no application associated with the given file name extension.");
-        } else if (errorCode == SE_ERR_OOM)
-        {
-            return std::make_tuple(false, "ShellExecuteA failed: There was not enough memory to complete the operation.");
-        } else if (errorCode == SE_ERR_SHARE)
-        {
-            return std::make_tuple(false, "ShellExecuteA failed: A sharing violation occurred.");
-        }
-        return std::make_tuple(false, "ShellExecuteA failed: Unknown error.");
+  aww::result_t launchFile(const std::string &path)
+  {
+    // check path is null
+    if (path.empty())
+    {
+      return std::make_tuple(false, "Argument path is empty");
     }
+    HINSTANCE execResult = ShellExecuteA(nullptr, "open", path.c_str(), nullptr, nullptr, SW_SHOWNORMAL);
+    const HINSTANCE Success = reinterpret_cast<HINSTANCE>(33);
+
+    if (execResult >= Success)
+    {
+      return std::make_tuple(true, "");
+    }
+
+    unsigned long errorCode = GetLastError();
+
+    if (errorCode == 0)
+    {
+      return std::make_tuple(false, "ShellExecuteA failed: The operating system is out of memory or resources.");
+    }
+    else if (errorCode == ERROR_FILE_NOT_FOUND)
+    {
+      return std::make_tuple(false, "ShellExecuteA failed: The specified file was not found.");
+    }
+    else if (errorCode == ERROR_PATH_NOT_FOUND)
+    {
+      return std::make_tuple(false, "ShellExecuteA failed: The specified path was not found.");
+    }
+    else if (errorCode == ERROR_BAD_FORMAT)
+    {
+      return std::make_tuple(false, "ShellExecuteA failed: The .exe file is invalid (non-Win32 .exe or error in .exe image).");
+    }
+    else if (errorCode == SE_ERR_ACCESSDENIED)
+    {
+      return std::make_tuple(false, "ShellExecuteA failed: The operating system denied access to the specified file.");
+    }
+    else if (errorCode == SE_ERR_ASSOCINCOMPLETE)
+    {
+      return std::make_tuple(false, "ShellExecuteA failed: The file name association is incomplete or invalid.");
+    }
+    else if (errorCode == SE_ERR_DDEBUSY)
+    {
+      return std::make_tuple(false, "ShellExecuteA failed: The DDE transaction could not be completed because other DDE transactions were being processed.");
+    }
+    else if (errorCode == SE_ERR_DDEFAIL)
+    {
+      return std::make_tuple(false, "ShellExecuteA failed: The DDE transaction failed.");
+    }
+    else if (errorCode == SE_ERR_DDETIMEOUT)
+    {
+      return std::make_tuple(false, "ShellExecuteA failed: The DDE transaction could not be completed because the request timed out.");
+    }
+    else if (errorCode == SE_ERR_DLLNOTFOUND)
+    {
+      return std::make_tuple(false, "ShellExecuteA failed: The specified DLL was not found.");
+    }
+    else if (errorCode == SE_ERR_NOASSOC)
+    {
+      return std::make_tuple(false, "ShellExecuteA failed: There is no application associated with the given file name extension.");
+    }
+    else if (errorCode == SE_ERR_OOM)
+    {
+      return std::make_tuple(false, "ShellExecuteA failed: There was not enough memory to complete the operation.");
+    }
+    else if (errorCode == SE_ERR_SHARE)
+    {
+      return std::make_tuple(false, "ShellExecuteA failed: A sharing violation occurred.");
+    }
+    return std::make_tuple(false, "ShellExecuteA failed: Unknown error.");
+  }
 }
