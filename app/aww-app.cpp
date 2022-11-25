8861412754dfce3fb7994397749b9413f2547654
dzhariy <you@example.com>
2022-11-24 22:01:34 -0800

    add aww-app

---
+++ app/aww-app.cpp
@@ -0,0 +1,22 @@
+// Executables must have the following defined if the library contains
+// doctest definitions. For builds with this disabled, e.g. code shipped to
+// users, this can be left out.
+#ifdef ENABLE_DOCTEST_IN_LIBRARY
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest/doctest.h"
+#endif
+
+#include <iostream>
+#include <string>
+
+#include "aww-common.hpp"
+
+int main(int argc, char **argv)
+{
+  std::vector<std::string> cmdArgs(argv, argv + argc);
+  cmdArgs.erase(cmdArgs.begin()); // remove first element
+
+  std::cout << "Hello World!" << std::endl;
+
+  return 0;
+}
---
+++ webview-webview.md
@@ -0,0 +1,7 @@
+[webview/webview](https://github.com/webview/webview)
+
+Debian-based systems:
+
+Packages:
+    - Development: apt install libgtk-3-dev libwebkit2gtk-4.0-dev
+    - Production: apt install libgtk-3-0 libwebkit2gtk-4.0-37
