ca8635dc41f1dee219a33d101b395ab7b0911955
Gogs <gogs@fake.local>
2022-12-17 13:08:47 -0800

    add nlohmann/json

--- app/aww-app.cpp
+++ app/aww-app.cpp
@@ -7,17 +7,36 @@
 #endif
 
 #include <iostream>
+#include <fstream>
 #include <string>
 
 #include "aww-common.hpp"
 #include "webview.h"
+#include "nlohmann-json.hpp"
 
-int main(int argc, char **argv)
-{
-  std::vector<std::string> cmdArgs(argv, argv + argc);
-  cmdArgs.erase(cmdArgs.begin()); // remove first element
+namespace fs = std::filesystem;
 
-  std::cout << "Hello World!" << std::endl;
+#ifdef _WIN32
+// TODO: it compiles, but how do I hide the console window?
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+#else
+int main() {
+#endif
+  const fs::path myPath = fs::absolute(fs::current_path());
+  const fs::path appmanifestJsonFile = myPath / "appmanifest.json";
+
+  if (!fs::exists(appmanifestJsonFile))
+  {
+    std::cout << "appmanifest.json not found" << std::endl;
+    return 1;
+  }
+
+  std::ifstream appmanifestFile(appmanifestJsonFile);
+  nlohmann::json appmanifestJson = nlohmann::json::parse(appmanifestFile);
+
+  // log appmanifestJson["description"]
+  std::cout << "appmanifestJson[\"description\"]: " << appmanifestJson["description"] << std::endl;
+
 
   std::string myCommandlineArgs = aww::string::join(aww::os::getCommandLineArgs(), "<br />");
   std::cout << "myCommandlineArgs: " << myCommandlineArgs << std::endl;
