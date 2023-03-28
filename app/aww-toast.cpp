c682d8095b2c7db32693febf1eb5bd24a47b87e4
Dmytro Zharii <you@example.com>
2023-03-27 18:02:43 -0700

    completed migration to lib, except aww-app that needs rewrite

--- app/aww-toast.cpp
+++ app/aww-toast.cpp
@@ -1,34 +1,6 @@
-// Executables must have the following defined if the library contains
-// doctest definitions. For builds with this disabled, e.g. code shipped to
-// users, this can be left out.
-#ifdef ENABLE_DOCTEST_IN_LIBRARY
-#define DOCTEST_CONFIG_IMPLEMENT
-#include "doctest/doctest.h"
-#endif
-
-#include <iostream>
-#include <string>
-#include <vector>
-#include <filesystem>
-
-#include "exampleConfig.h"
-#include "aww-common.hpp"
+#include "internal/aww-toast.hpp"
 
 int main(int argc, char **argv)
 {
-  std::vector<std::string> cmdArgs(argv, argv + argc);
-  cmdArgs.erase(cmdArgs.begin()); // remove first element
-  std::string message = "Hey!";
-  if (cmdArgs.size() > 0)
-  {
-    message = aww::string::join(cmdArgs, " ");
-  }
-  aww::Result result = aww::os::actions::showNotification("AwwTools", message);
-  if (result.isOk()) {
-    std::cout << "Notification sent" << "\n";
-  } else {
-    std::cout << "Failed to send notification" << "\n";
-  }
-
-  return 0;
+  return aww::internal::aww_toast::aww_toast_main(argc, argv);
 }
---
+++ include/internal/aww-toast.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef AWW_TOAST_HPP
+#define AWW_TOAST_HPP
+
+namespace aww::internal::aww_toast
+{
+  int aww_toast_main(int argc, char **argv);
+}
+
+#endif // AWW_TOAST_HPP
