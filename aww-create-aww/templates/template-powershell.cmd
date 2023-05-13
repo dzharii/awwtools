e08f5e8c72c4a1d9fd4ff777386ca5f103be4746
Dmytro Zharii <dmytrozharii@microsoft.com>
2023-05-13 10:51:37 -0700

    update Sat 05/13/2023

--- aww-create-aww/templates/template-powershell.cmd
+++ aww-create-aww/templates/template-powershell.cmd
@@ -1,3 +1,3 @@
 @rem Example Usage:
 @rem runs powershell (v5) script with same name as this .cmd file
-@powershell -executionpolicy unrestricted -File %~dp0%~n0.ps1 %*
+@powershell -executionpolicy unrestricted -File "%~dp0%~n0.ps1" %*
--- aww-create-aww/templates/template-pwsh.cmd
+++ aww-create-aww/templates/template-pwsh.cmd
@@ -1,3 +1,3 @@
 @rem Example Usage:
 @rem runs pwsh powershell script with same name as this .cmd file
-@pwsh -executionpolicy unrestricted -File %~dp0%~n0.ps1 %*
+@pwsh -executionpolicy unrestricted -File "%~dp0%~n0.ps1" %*
