5aff24eb2e569c4deec8654473bed2ba1e1ba61b
Dmytro Zharii <dmytrozharii@microsoft.com>
2023-05-13 10:34:56 -0700

    update Sat 05/13/2023

--- aww-create-aww/templates/template.ps1
+++ aww-create-aww/templates/template.ps1
@@ -5,8 +5,8 @@ param(
 
 $ErrorActionPreference = "Stop"
 
-$host.ui.RawUI.WindowTitle = "My Title"
-$ThisScriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+# $host.ui.RawUI.WindowTitle = "My Title"
+$ThisScriptFolderPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 Write-Host "Hello World"
 
