48d3bb40a4ef6011a1b92b9c1ff5b62eb31b77be
Dmytro Zharii <dmytrozharii@microsoft.com>
2023-06-05 21:27:29 -0700

    update Mon 06/05/2023

--- tests/test-aww-create.cpp
+++ tests/test-aww-create.cpp
@@ -8,92 +8,91 @@
 
 class aww_create_io_dependencies_stub : public aww::internal::aww_create::aww_create_io_dependencies_interface {
 public:
-  std::string get_date_yyyymmdd_returns = "2023-06-02";
+  std::function<std::string(int)> get_date_yyyymmdd_stub = [this](int count) {
+    return "2023-06-02";
+  };
   int get_date_yyyymmdd_called = 0;
 
-  bool fs_exists_returns = true;
-  int fs_exists_called = 0;
-  bool fs_exists_outExists = false;
+  std::function<std::filesystem::path()> fs_get_current_executable_path_stub = [this]() {
+    return std::filesystem::path();
+  };
+  int fs_get_current_executable_path_called = 0;
 
-  aww::Result fs_create_directories_returns = aww::Result::ok();
+  std::function<aww::Result(int, const std::filesystem::path&, bool&)> fs_exists_stub = [this](int callCount, const std::filesystem::path& target, bool& outFileExists) -> aww::Result {
+    outFileExists = fs_exists_outExists;
+    return aww::Result::ok();
+  };
+  int fs_exists_called = 0;
+
+  std::function<aww::Result(int, const std::filesystem::path&)> fs_create_directories_stub = [this](int callCount, const std::filesystem::path& path) -> aww::Result {
+    return aww::Result::ok();
+  };
   int fs_create_directories_called = 0;
 
-  aww::Result fs_create_empty_file_returns = aww::Result::ok();
+  std::function<aww::Result(int, const std::filesystem::path&)> fs_create_empty_file_stub = [this](int callCount, const std::filesystem::path& path) -> aww::Result {
+    return aww::Result::ok();
+  };
   int fs_create_empty_file_called = 0;
 
-  aww::Result fs_read_lines_returns = aww::Result::ok();
+  std::function<aww::Result(int, const std::filesystem::path&, std::vector<std::string>&)> fs_read_lines_stub = [this](int callCount, const std::filesystem::path& filePath, std::vector<std::string>& outFileLines) -> aww::Result {
+    return aww::Result::ok();
+  };
   int fs_read_lines_called = 0;
-  std::vector<std::string> fs_read_lines_outLines;
 
-  aww::Result fs_write_lines_returns = aww::Result::ok();
+  std::function<aww::Result(int, const std::filesystem::path&, const std::vector<std::string>&)> fs_write_lines_stub = [this](int callCount, const std::filesystem::path& filePath, const std::vector<std::string>& lines) -> aww::Result {
+    fs_write_lines_inLines = lines;
+    return aww::Result::ok();
+  };
   int fs_write_lines_called = 0;
-  std::vector<std::string> fs_write_lines_inLines;
 
-  std::string show_notification_last_title = "";
-  std::string show_notification_last_message = "";
+  std::function<void(const std::string&, const std::string&)> show_notification_stub = [this](const std::string& title, const std::string& message) {
+
+  };
+
   int show_notification_called = 0;
 
   std::string get_date_yyyymmdd() override {
     get_date_yyyymmdd_called += 1;
-    return get_date_yyyymmdd_returns;
+    return get_date_yyyymmdd_stub(get_date_yyyymmdd_called);
   }
 
-  inline std::filesystem::path fs_get_current_executable_path() override {
-    return std::filesystem::path();
+  std::filesystem::path fs_get_current_executable_path() override {
+    fs_get_current_executable_path_called += 1;
+    return fs_get_current_executable_path_stub();
   }
 
-  inline aww::Result fs_exists(const std::filesystem::path& target, bool& outFileExists) override {
+  aww::Result fs_exists(const std::filesystem::path& target, bool& outFileExists) override {
     fs_exists_called += 1;
-    outFileExists = fs_exists_outExists;
-    return aww::Result::ok();
+    return fs_exists_stub(fs_exists_called, target, outFileExists);
   }
 
-  inline aww::Result fs_create_directories(const std::filesystem::path& path) override {
+  aww::Result fs_create_directories(const std::filesystem::path& path) override {
     fs_create_directories_called += 1;
-    return fs_create_directories_returns;
+    return fs_create_directories_stub(fs_create_directories_called, path);
   }
 
-  inline aww::Result fs_create_empty_file(const std::filesystem::path& path) override {
+  aww::Result fs_create_empty_file(const std::filesystem::path& path) override {
     fs_create_empty_file_called += 1;
-    return fs_create_empty_file_returns;
+    return fs_create_empty_file_stub(fs_create_empty_file_called, path);
   }
 
-  inline aww::Result fs_read_lines(const std::filesystem::path& filePath, std::vector<std::string>& outFileLines) override {
+  aww::Result fs_read_lines(const std::filesystem::path& filePath, std::vector<std::string>& outFileLines) override {
     fs_read_lines_called += 1;
-    outFileLines = fs_read_lines_outLines;
-    return fs_read_lines_returns;
+    return fs_read_lines_stub(fs_read_lines_called, filePath, outFileLines);
   }
 
-  inline aww::Result fs_write_lines(const std::filesystem::path& filePath, const std::vector<std::string>& lines) override {
+  aww::Result fs_write_lines(const std::filesystem::path& filePath, const std::vector<std::string>& lines) override {
     fs_write_lines_called += 1;
-    fs_write_lines_inLines = lines;
-    return fs_write_lines_returns;
+    return fs_write_lines_stub(fs_write_lines_called, filePath, lines);
   }
 
   void show_notification(const std::string& title, const std::string& message) override {
     show_notification_called += 1;
-    show_notification_last_title = title;
-    show_notification_last_message = message;
+    show_notification_stub(title, message);
   }
 };
 
+
 TEST_CASE("aww::internal::aww_create::aww_create_main: create directory") {
-  // Test case setup
-  std::string directoryPath = "test_directory";
-  std::vector<std::string> cmdArgs = {directoryPath};
-  aww_create_io_dependencies_stub stubDependencies;
 
-  // Execute the function
-  int mainResult = aww::internal::aww_create::aww_create_main(cmdArgs, stubDependencies);
-
-  // Assertions
-  CHECK_MESSAGE(mainResult == 0, "Expected aww_create_main to return 0, but it returned " << mainResult);
-
-  bool directoryExists = false;
-  aww::Result fsExistsResult = stubDependencies.fs_exists(directoryPath, directoryExists);
-  CHECK_MESSAGE(fsExistsResult.isOk(), "Failed to check if directory exists: " << fsExistsResult.error());
-  CHECK_MESSAGE(directoryExists, "Expected directory to be created, but it doesn't exist");
-
-  // Cleanup
-  std::filesystem::remove(directoryPath);
 }
