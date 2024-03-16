c9c836927799f4a56b0965c79e7dca01f4c43a2e
Your Name <you@example.com>
2024-03-16 11:36:05 -0700

    update Sat Mar 16 11:36:05 AM PDT 2024

--- cmake-b.sh
+++ cmake-b.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 cmake -B cmake-build -S . -DCMAKE_BUILD_TYPE=RELEASE &&\
 cmake --build cmake-build &&\
-cmake-build\Debug\unit_tests.exe &&\
-cmake-build\Debug\unit_tests.exe --list-test-cases
+./cmake-build/unit_tests &&\
+./cmake-build/unit_tests --list-test-cases
