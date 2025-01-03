d010b9ccd8fcd5e29da62212bff7982ad3d01aed
John Doe <johndoe@example.com>
2025-01-02 23:36:38 -0800

    add cmake j flag, I hope it works

--- cmake-b.sh
+++ cmake-b.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 cmake -B build -S . -DCMAKE_BUILD_TYPE=RELEASE &&\
-cmake --build build &&\
+cmake --build build -j --config Release &&\
 ./build/unit_tests &&\
 ./build/unit_tests --list-test-cases
