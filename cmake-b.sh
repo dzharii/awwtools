c1772152d4a608b463b1db6bd315611fe94ef196
John Doe <johndoe@example.com>
2025-07-04 20:03:15 -0700

    upd

--- cmake-b.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-#!/bin/env bash
-source read-env-file.sh
-BUILD_DIR=${BUILD_DIR_BASE}-linux-release
-cmake -B $BUILD_DIR -S . -DCMAKE_BUILD_TYPE=RELEASE &&\
-cmake --build $BUILD_DIR -j --config Release &&\
-./$BUILD_DIR/bin/unit_tests &&\
-./$BUILD_DIR/bin/unit_tests --list-test-cases
