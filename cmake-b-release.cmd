623fe738e6c2047d7150a176421d249148d948ac
John Doe <johndoe@example.com>
2026-03-28 00:08:14 -0700

    add arm64 support

--- cmake-b-release.cmd
+++ cmake-b-release.cmd
@@ -1,8 +1,11 @@
 @echo off
 call read-env-file.cmd
 
-set BUILD_DIR=%BUILD_DIR_BASE%-windows-release
-cmake -B %BUILD_DIR% -S . -DCMAKE_BUILD_TYPE=RELEASE && ^
+if "%AWW_WINDOWS_ARCH%"=="" set "AWW_WINDOWS_ARCH=x64"
+if "%AWW_CMAKE_GENERATOR_PLATFORM%"=="" set "AWW_CMAKE_GENERATOR_PLATFORM=x64"
+
+set BUILD_DIR=%BUILD_DIR_BASE%-windows-%AWW_WINDOWS_ARCH%-release
+cmake -B %BUILD_DIR% -S . -A %AWW_CMAKE_GENERATOR_PLATFORM% -DCMAKE_BUILD_TYPE=RELEASE && ^
 call cmake --build %BUILD_DIR% -j --config Release && ^
 call .\%BUILD_DIR%\bin\unit_tests.exe && ^
 call .\%BUILD_DIR%\bin\unit_tests.exe --list-test-cases
